1fbe618b52647bdeee9ddccb23419fa2b773eee2
astherath <farceriv@gmail.com>
2020-02-01 17:29:44 +0000

    starting frontend devlopment now

--- data_vis/activity_monitor.py
+++ data_vis/activity_monitor.py
@@ -1,5 +1,6 @@
 import tqdm
 import pymongo
+import pandas as pd
 import matplotlib.pyplot as plt
 
 
@@ -8,33 +9,23 @@ client = pymongo.MongoClient(URL)
 db = client.gc_data
 col = db.messages
 
-def write_to_file():
-    names = ['Rafa', 'Quinn', 'Matias', 'Martin', 'Liam', 'Luke', 'Cynder', 'Babak', 'Collin', 'Davo', 'Evan', 'Hannan', 'Israel', 'Jimmy', 'John', 'Felipe']
 
-    with open('full_text.txt', 'w') as f:
-        text = col.find({"Author": {'$ne': "System message"}}, {"_id": 0, "Body":1})
-        all_mess = text.count()
-        full_text = ""
-        num = 0
-        with tqdm.tqdm(total=all_mess) as pbar:
-            for mess in text:
-                message = mess['Body']
-                f.write(message + ' ')
-                num = num + 1
-                pbar.update(1)
-def get_all_words():
-    return open('full_text.txt', 'r').read()
-def generate_wordcloud(text):
-    wordcloud = WordCloud(max_font_size=150, width=2000, height=1200).generate(text)
-    # Open a plot of the generated image.
-    plt.figure()
-    plt.imshow(wordcloud)
-    plt.axis("off")
-    plt.tight_layout(pad=0)
-    plt.savefig('cloud.png', bbox_inches='tight')
+
+def add_all_names(names):
+    dates = ['2019-01','2019-02','2019-03','2019-04']
+    df_dict = {'Dates': dates, 'Names': names}
+    df = pd.DataFrame(df_dict)
+
+    #  for name in names:
+        #  df.loc[name] =
+
+    return df
 
 
 if __name__ == '__main__':
-    write_to_file()
-    text = get_all_words()
-    generate_wordcloud(text)
+    names = ['Rafa', 'Quinn', 'Matias', 'Martin', 'Liam', 'Luke', 'Cynder', 'Babak', 'Collin', 'Davo', 'Evan', 'Hannan', 'Israel', 'Jimmy', 'John', 'Felipe']
+
+    df = add_all_names(names)
+
+    print(df)
+
