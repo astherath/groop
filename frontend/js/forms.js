a1c3b407d8def72b554e8ee514484f8a40444ccf
astherath <farceriv@gmail.com>
2020-02-10 09:58:08 -0500

    more work on login form handlers

---
+++ frontend/js/forms.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('#login-submit').click(function(){
+        var user = document.getElementById("login-user").value;
+    	var pwd = document.getElementById("login-pwd").value;
+        var url = 'https://felipearce.pw:3000/login?username=' + user + '&pwd=' + pwd;
+        console.log(url)
+        let request = new XMLHttpRequest();
+        request.open('GET', url, true);
+        request.withCredentials = false;
+        request.responseType = 'json';
+        request.onload = function() {
+            console.log(request.response);
+        };
+        
+        request.send();
+        
+         return false;
+    });
+   
+});
