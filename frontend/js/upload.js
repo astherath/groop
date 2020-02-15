b93c135e4655b7f858b9f0c085f0a6d52c5b5945
astherath <farceriv@gmail.com>
2020-02-14 21:38:35 -0500

    more work on files

--- frontend/js/upload.js
+++ frontend/js/upload.js
@@ -1,210 +1,40 @@
-'use strict';
+$(document).ready(function(){
+    let doc = document.getElementById("file-upload").files[0];  // file from input
+    var url = 'https://groop.pw:3000/files/upload';
+    console.log(url);
+    let request = new XMLHttpRequest();
+    let formData = new FormData();
+    formData.append("file", doc);
+    request.open('POST', url, true);
+    request.withCredentials = false;
+    request.responseType = 'json';
+    request.onload = function() {
+        let res = request.response;
 
-( function ( document, window, index )
-{
-    // global vars
-    var isTxt = false;
-    var error = false;
-    let url = 'https://groop.pw:3000/files/upload'
-    // feature detection for drag&drop upload
-    var isAdvancedUpload = function()
-        {
-            var div = document.createElement( 'div' );
-            return ( ( 'draggable' in div ) || ( 'ondragstart' in div && 'ondrop' in div ) ) && 'FormData' in window && 'FileReader' in window;
-        }();
-
-
-    // applying the effect for every form
-    var forms = document.querySelectorAll( '.box' );
-    Array.prototype.forEach.call( forms, function( form )
-    {
-        var input		 = form.querySelector( 'input[type="file"]' ),
-            label		 = form.querySelector( 'label' ),
-            errorMsg	 = form.querySelector( '.box__error span' ),
-            restart		 = form.querySelectorAll( '.box__restart' ),
-            droppedFiles = false,
-            showFiles	 = function( files )
+        if (request.status == 200 && res.success)
             {
-                label.textContent = files.length > 1 ? ( input.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', files.length ) : files[ 0 ].name;
-                var ext = files[0].name.split('.').pop();
-                isTxt = (ext.localeCompare("txt") == 0);
-            },
-            triggerFormSubmit = function()
-            {
-                var event = document.createEvent( 'HTMLEvents' );
-                event.initEvent( 'submit', true, false );
-                form.dispatchEvent( event );
-            };
-
-        // letting the server side to know we are going to make an Ajax request
-        var ajaxFlag = document.createElement( 'input' );
-        ajaxFlag.setAttribute( 'type', 'hidden' );
-        ajaxFlag.setAttribute( 'name', 'ajax' );
-        ajaxFlag.setAttribute( 'value', 1 );
-        form.appendChild( ajaxFlag );
-
-        // automatically submit the form on file select
-        input.addEventListener( 'change', function( e )
-        {
-            showFiles( e.target.files );
-
-
-            triggerFormSubmit();
-
-
-        });
-
-        // drag&drop files if the feature is available
-        if( isAdvancedUpload )
-        {
-            form.classList.add( 'has-advanced-upload' ); // letting the CSS part to know drag&drop is supported by the browser
-
-            [ 'drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop' ].forEach( function( event )
-            {
-                form.addEventListener( event, function( e )
-                {
-                    // preventing the unwanted behaviours
-                    e.preventDefault();
-                    e.stopPropagation();
-                });
-            });
-            [ 'dragover', 'dragenter' ].forEach( function( event )
-            {
-                form.addEventListener( event, function()
-                {
-                    form.classList.add( 'is-dragover' );
-                });
-            });
-            [ 'dragleave', 'dragend', 'drop' ].forEach( function( event )
-            {
-                form.addEventListener( event, function()
-                {
-                    form.classList.remove( 'is-dragover' );
-                });
-            });
-            form.addEventListener( 'drop', function( e )
-            {
-                droppedFiles = e.dataTransfer.files; // the files that were dropped
-                showFiles( droppedFiles );
-
-
-                triggerFormSubmit();
-
-                                });
-        }
-
-
-        // if the form was submitted
-        form.addEventListener( 'submit', function( e )
-        {
-            // dont be redundant with the alert
-            $("#error-alert").hide();
-            // preventing the duplicate submissions if the current one is in progress
-            if( form.classList.contains( 'is-uploading' ) ) return false;
-
-            form.classList.add( 'is-uploading' );
-            form.classList.remove( 'is-error' );
-
-            if( isAdvancedUpload ) // ajax file upload for modern browsers
-            {
-                e.preventDefault();
-
-                // gathering the form data
-                var ajaxData = new FormData( form );
-                if( droppedFiles )
-                {
-                    Array.prototype.forEach.call( droppedFiles, function( file )
-                    {
-                        ajaxData.append( input.getAttribute( 'name' ), file );
-                        
-                    });
-                    
-                }
-
-                // ajax request
-                var ajax = new XMLHttpRequest();
-                ajax.open( 'POST', url, true );
-                ajax.responseType = 'json';
-                console.log('url: ' + form.getAttribute( 'action' ))
-
-                ajax.onload = function()
-                {
-                    console.log(ajax.response);
-                    form.classList.remove( 'is-uploading' );
-                    if (!isTxt)
-                        {
-                            document.getElementById("error-alert").innerHTML = "Please upload a .txt file.";
-                            document.getElementById("error-alert").style = ""
-                            alert( 'Please upload a .txt file.' );
-                        }
-                    else if( ajax.status >= 200 && ajax.status < 400 && isTxt)
-                    {
-                        var data = JSON.parse( ajax.responseText );
-                        form.classList.add( data.success == true ? 'is-success' : 'is-error' );
-                        if( !data.success ) errorMsg.textContent = data.error;
-                    }
-                    else
-                        {
-                            console.log(ajax.response.error);
-                            document.getElementById("error-alert").innerHTML = "Error. Please, contact the webmaster!";
-                            $("#error-alert").show();
-                            alert( 'Error. Please, contact the webmaster!' );
-                        }
-                    
-                };
-
-                ajax.onerror = function()
-                {
-                    form.classList.remove( 'is-uploading' );
-                    document.getElementById("error-alert").innerHTML = "Error. Please, try again!";
-                    $("#error-alert").show();
-                    alert( 'Error. Please, try again!' );
-                };
-
-                ajax.send( ajaxData );
+//                    window.location.replace("https://groop.pw/dashboard.html");
+                console.log('all good');
             }
-            else // fallback Ajax solution upload for older browsers
+        else if (request.status != 500)
             {
-                var iframeName	= 'uploadiframe' + new Date().getTime(),
-                    iframe		= document.createElement( 'iframe' );
-
-                    $iframe		= $( '<iframe name="' + iframeName + '" style="display: none;"></iframe>' );
-
-                iframe.setAttribute( 'name', iframeName );
-                iframe.style.display = 'none';
-
-                document.body.appendChild( iframe );
-                form.setAttribute( 'target', iframeName );
-
-                iframe.addEventListener( 'load', function()
-                {
-                    var data = JSON.parse( iframe.contentDocument.body.innerHTML );
-                    form.classList.remove( 'is-uploading' )
-                    form.classList.add( data.success == true ? 'is-success' : 'is-error' )
-                    form.removeAttribute( 'target' );
-                    if( !data.success ) errorMsg.textContent = data.error;
-                    iframe.parentNode.removeChild( iframe );
-                });
+//                document.getElementById("error-text").innerHTML = res.error;
+                console.log("500 error");
             }
-        });
-
-
-        // restart the form if has a state of error/success
-        Array.prototype.forEach.call( restart, function( entry )
-        {
-            // dont be redundant with the alert
-            $("#error-alert").hide();
-            entry.addEventListener( 'click', function( e )
+        else
             {
-                e.preventDefault();
-                form.classList.remove( 'is-error', 'is-success' );
-                input.click();
-            });
-        });
+//                document.getElementById("error-text").innerHTML = "Server error, please try again later";
+                console.log("Server error, please try again later");
+            }
+    };
+//    if (!error)
+//        {
+//            request.send(formData);
+//        }
+    request.send(formData);
+    return false;
+});
 
-        // Firefox focus bug fix for file input
-        input.addEventListener( 'focus', function(){ input.classList.add( 'has-focus' ); });
-        input.addEventListener( 'blur', function(){ input.classList.remove( 'has-focus' ); });
 
-    });
-}( document, window, 0 ));
+
+
