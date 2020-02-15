60bc2b19b3cf1c90efe66b94ab080701026ca504
astherath <farceriv@gmail.com>
2020-02-15 02:38:00 +0000

    started work on file handling

---
+++ file_ops/files.py
@@ -0,0 +1,28 @@
+import os
+from flask import request, Blueprint, make_response, jsonify
+from werkzeug.utils import secure_filename
+
+files = Blueprint('files', __name__)
+
+@files.route('/files/upload', methods=['POST'])
+def upload_file():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        resp = jsonify({'message' : 'No file part in the request'})
+        resp.status_code = 400
+        return resp
+    file = request.files['file']
+    if file.filename == '':
+        resp = jsonify({'message' : 'No file selected for uploading'})
+        resp.status_code = 400
+        return resp
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        resp = jsonify({'message' : 'File successfully uploaded'})
+        resp.status_code = 201
+        return resp
+    else:
+        resp = jsonify({'message' : 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
+        resp.status_code = 400
+        return resp
